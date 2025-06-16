a4e18a73fd3e99d60389bb5379bd3fc51e845bee
Zakhar Veremchuk <zaharverem@gmail.com>
2025-06-16 21:35:58 +0300

    Finished

--- src/main.js
+++ src/main.js
@@ -5,29 +5,44 @@ import { renderGallery } from './js/render-functions';
 
 const inputField = document.querySelector(".searchfield");
 const form = document.querySelector(".inputform");
+const customLoader = document.getElementById('custom-loader');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     const loadingMessage = document.getElementById('loading-message');
-    loadingMessage.style.display = 'block';
+    const query = event.target.elements.searchfield.value.trim();
+    const gallery = document.querySelector('.gallery');
+    customLoader.style.display = 'block';
 
-    const query = inputField.value.trim();
+    if (!query) {
+        iziToast.warning({
+            message: 'Please enter a search query!',
+            position: 'topRight'
+        });
+        loadingMessage.style.display = 'none';
+        return;
+    }
+
+    loadingMessage.style.display = 'block';
+    gallery.innerHTML = '';
 
     fetchImages(query)
         .then((data) => {
+            customLoader.style.display = 'none';
             loadingMessage.style.display = 'none';
             if(data.hits.length == 0) {
+                event.target.elements.searchfield.value = '';
                 iziToast.error({
                     message: 'Sorry, there are no images mathcing your search query. Please try again!',
                     position: 'topRight'
                 })
             } else {
-                const gallery = document.querySelector('.gallery');
                 renderGallery(gallery, data.hits);
                 lightbox.refresh();
             }
         })
         .catch(() => {
+            customLoader.style.display = 'none';
             loadingMessage.style.display = 'none';
         });
 })
